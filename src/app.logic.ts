bd4b0d1f6d144f09e691c64e6872fa3ba932f2b3
Jorge <jorgereyeslira02@gmail.com>
2024-09-09 23:40:38 -0500

    Fin seccion 6

---
+++ src/app.logic.ts
@@ -0,0 +1,28 @@
+import fs from 'fs'
+import { yarg } from './config/plugins/yargs.plugin';
+
+
+
+const {b:base,l:limit,s:showTable} = yarg;
+
+
+let outputMessage = "";
+const headerMessage = `
+Tabla del ${base}
+
+`
+
+for(let i = 1; i<=limit; i++){
+    outputMessage += `${base} x ${i} = ${base * i}\n`;
+
+}
+const outputPath ='outputs/'
+
+
+outputMessage = headerMessage + outputMessage;
+if(showTable){
+    console.log(outputMessage)
+}
+fs.mkdirSync(outputPath, {recursive: true})
+fs.writeFileSync(`${outputPath}/tabla-${base}.txt`, outputMessage);
+console.log("File Created")
