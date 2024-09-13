04a085839ffa5dc819b0987a114f6f0f480155ea
Jorge <jorgereyeslira02@gmail.com>
2024-09-13 16:56:08 -0500

    Pruebas en app.ts

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,5 +1,24 @@
-describe('first', () => { 
-    test('should first', () => { 
-        expect(true).toBe(true)
+//import { argv } from "process"
+import { ServerApp } from "./presentation/server-app"
+
+
+describe('Test App.ts', () => {
+    test('should call Server with custom values', async() => {
+
+        const serverRunMock = jest.fn()
+        ServerApp.run = serverRunMock
+        
+        process.argv = ['node','app.ts','-b','10','-l','5','-s','true','-n','test.txt','-d','outputs']
+        console.log(process.argv)
+
+        await import('./app');
+        expect(serverRunMock).toHaveBeenCalledWith({
+            base: 10,
+            limit: 5,
+            showTable: true,
+            fileName: 'test.txt',
+            fileDestination: 'outputs'
+        })
+
     })
-     })
+})
