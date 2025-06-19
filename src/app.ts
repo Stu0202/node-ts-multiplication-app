cb93d170c7803e7172de094e8a41cfb1513ef4b0
Jorge <jorgereyeslira02@gmail.com>
2025-06-18 21:50:48 -0500

    Agregando opciones a los yargs para construir nuestra tabla de multiplicar

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,15 @@
 import {yarg} from './config/plugins/yargs.plugin'
 
-console.log(process.argv)
+//console.log(process.argv)
 
-console.log(yarg)
+//console.log(yarg.b)
+
+( async() => {
+    await main()
+   
+})();
+
+async function main() {
+    console.log(yarg)
+}
 
--- src/config/plugins/yargs.plugin.ts
+++ src/config/plugins/yargs.plugin.ts
@@ -1,8 +1,28 @@
 //El paquete yargs sirve para que tu programa en Node.js pueda leer y entender argumentos desde la línea de comandos de forma fácil.
 //Soporta alias (-n en lugar de --nombre)
 
-import yargs from 'yargs'
+import yargs, { options } from 'yargs'
 import { hideBin } from 'yargs/helpers';
 
 export const yarg = yargs(process.argv)
-    .parseSync()
+
+//El option me permite decirle que hacer a node cuando le mando la bandera 'b'
+    .option('b',{
+        alias: 'base', 
+        type: 'number', //tipo de valor
+        demandOption: true, // es obligatorio mandar la base si no, no ejecuta la consola
+        describe:' Multiplication table base '
+    })
+    .option('l',{
+        alias: 'limit',
+        type:'number',
+        default:10, //Por defecto si no se lo manda por consola
+        describe:'Multiplication table limit'
+    })
+    .option('s',{
+        alias:'show',
+        type: 'boolean',
+        default: false,
+        describe: 'Show multiplication table'
+    })
+    .parseSync() // hace que yargs lea y devuelva los argumentos de la terminal ya validados y convertidos. Sin él
